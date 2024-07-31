09cba5df8dc0f9e39bf7272685f6910f455734b9
허건호 <gj1563@naver.com>
2024-07-31 15:40:58 +0900

    feature: 프로젝트 생성
    프로젝트 생성

---
+++ ERPSystem-Phase2-Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './styles/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          ASDASD
+      </div>
+    </>
+  )
+}
+
+export default App
