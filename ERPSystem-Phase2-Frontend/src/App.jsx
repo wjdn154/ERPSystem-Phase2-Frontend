10fa8b689854a867367aaa2cbe75e04e1b7e7b40
허건호 <gj1563@naver.com>
2024-07-31 16:21:35 +0900

    feature: 모듈 디렉토리 추가
    모듈 디렉토리 추가

--- ERPSystem-Phase2-Frontend/src/App.jsx
+++ ERPSystem-Phase2-Frontend/src/App.jsx
@@ -1,14 +1,33 @@
+import React from "react";
 import { useState } from 'react'
 import './styles/App.css'
+
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+import { AppBar, Tabs, Tab } from "@material-ui/core";
+
+const TopHeader = () => {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-          ASDASD
-      </div>
+        <AppBar position="static">
+            <Tabs value={value} onChange={handleChange} aria-label="Main Tabs">
+                <Tab label="Home" />
+                <Tab label="Send" />
+                <Tab label="Profile" />
+            </Tabs>
+        </AppBar>
     </>
   )
 }
