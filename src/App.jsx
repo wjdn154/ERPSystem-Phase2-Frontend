43e7bc3fa97233863c84a68c76e0641c4cebc672
허건호 <gj1563@naver.com>
2024-08-19 21:16:20 +0900

    feature: ui 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import './styles/App.css'; // 앱의 전역 스타일을 가져옴
 import React, { useState } from 'react'; // React 및 useState 훅을 가져옴
 import { CssBaseline, Box } from '@mui/material'; // Material-UI의 기본 컴포넌트 가져옴
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ContentWrapper from './modules/integration/components/ContentWrapper.jsx'; // ContentWrapper 컴포넌트 가져옴
 import Sidebar from './modules/integration/components/Sidebar.jsx'; // Sidebar 컴포넌트 가져옴
 import MainContent from './modules/integration/pages/MainContent.jsx'; // MainContent 컴포넌트 가져옴
 import Header from './modules/integration/components/Header'; // Header 컴포넌트 가져옴
 import { menuItems, subMenuItems } from './modules/integration/utils/menuItems'; // 메뉴 데이터 가져옴
 
+const theme = createTheme({
+    typography: {
+        fontFamily: 'NotoSansKR',
+    },
+});
+
 function App() {
     // 상태 선언: 현재 선택된 대분류, 중분류, 소분류 메뉴를 관리함
-    const [selectedMenu, setSelectedMenu] = useState('마이페이지');
+    const [selectedMenu, setSelectedMenu] = useState('그룹웨어');
     const [selectedSubMenu, setSelectedSubMenu] = useState('기초정보관리');
     const [selectedSubSubMenu, setSelectedSubSubMenu] = useState('회사정보수정');
 
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <CssBaseline /> {/* Material-UI의 기본 스타일을 초기화하여 브라우저 간 일관성을 유지 */}
             <Header
                 selectedMenu={selectedMenu} // 현재 선택된 대분류 메뉴 상태
@@ -42,7 +49,7 @@ function App() {
                     </ContentWrapper>
                 </Box>
             </Box>
-        </>
+        </ThemeProvider>
     );
 }
 
