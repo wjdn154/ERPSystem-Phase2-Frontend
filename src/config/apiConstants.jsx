872efb5aa2cbf222df20fd43f73aaf24f32542ce
Minseong <ckacl2512@naver.com>
2024-09-02 19:31:27 +0900

    feature: 사용자 페이지 생성 중
    사용자 페이지 생성 중

--- src/config/apiConstants.jsx
+++ src/config/apiConstants.jsx
@@ -10,7 +10,7 @@ export const FINANCIAL_API = {
     DELETE_ACCOUNT_SUBJECT_API: (code) => `${API_BASE_URL}/api/financial/accountSubjects/delete/${code}`, // 계정과목 삭제 API
 };
 
-// 인사관리
+// 인사관리 - 사원
 export const EMPLOYEE_API = {
     EMPLOYEE_DATA_API: `${API_BASE_URL}/api/hr/employee/all`, // 사원 목록 조회 API
     EMPLOYEE_DATA_DETAIL_API:(id) =>`${API_BASE_URL}/api/employee/${id}`, // 사원 상세 조회 API
@@ -18,6 +18,14 @@ export const EMPLOYEE_API = {
     UPDATE_EMPLOYEE_DATA_API:(id)=> `${API_BASE_URL}/employee/updateEmployee/${id}`, // 사원 수정 API
     DELETE_EMPLOYEE_DATA_API:(id)=> `${API_BASE_URL}/api/employee/del/${id}`,
 };
+// 인사관리 - 사용자
+export const USERS_API = {
+    USERS_DATA_API: `${API_BASE_URL}/api/hr/users/all`,
+    USERS_DATA_DETAIL_API: (id) => `${API_BASE_URL}/api/hr/users/${id}`,
+    SAVE_USERS_DATA_API: `${API_BASE_URL}/api/hr/users/create`,
+    UPDATE_USERS_DATA_API: (id)=> `${API_BASE_URL}/api/hr/users/put/${id}`,
+    DELETE_USERS_DATA_API: (id) =>`${API_BASE_URL}/api/hr/users/del/${id}`,
+}
 
 // 물류관리
 export const LOGISTICS_API = {
