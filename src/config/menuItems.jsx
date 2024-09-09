dc64c4b8217ebd0bb23591b70618585f76db2d4e
jh <chlwlgur0407@naver.com>
2024-09-10 02:23:00 +0900

    fix: 창고리스트 임포트 수정

--- src/config/menuItems.jsx
+++ src/config/menuItems.jsx
@@ -16,7 +16,6 @@ import AccountSubjectPage from "../modules/financial/pages/AccountSubjectPage.js
 import MainContentPage from "../modules/integration/pages/MainContentPage.jsx";
 import EquipmentDataPage from "../modules/production/pages/EquipmentDataPage.jsx";
 import MaintenanceHistoryPage from "../modules/production/pages/MaintenanceHistoryPage.jsx";
-import WarehouseListPage from "../modules/logistics/pages/WarehouseListPage.jsx";
 import EmployeeDataPage from "../modules/hr/pages/EmployeeDataPage.jsx";
 import UsersDataPage from "../modules/hr/pages/UsersDataPage.jsx";
 import WorkcenterPage from "../modules/production/pages/Workcenter/WorkcenterPage.jsx";
