35e4b6844c64bf1931a827748043d73efd7e7dcd
wjdn154 <wjdn154@gmail.com>
2024-09-15 06:39:06 +0900

    feature: ui 개선

--- src/config/apiConstants.jsx
+++ src/config/apiConstants.jsx
@@ -3,6 +3,7 @@ import Cookies from 'js-cookie';
 
 
 const API_BASE_URL = "http://localhost:8080";
+// const API_BASE_URL = "http://15.165.212.208:8080";
 
 
 axios.interceptors.request.use((config) => {
