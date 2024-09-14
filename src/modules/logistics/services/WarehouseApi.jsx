7130b311b7b2bd3353a7e122199e37f06535032a
jh <chlwlgur0407@naver.com>
2024-09-14 16:31:19 +0900

    fix: 창고 목록 조회 수정중

--- src/modules/logistics/services/WarehouseApi.jsx
+++ src/modules/logistics/services/WarehouseApi.jsx
@@ -10,26 +10,3 @@ export const fetchWarehouseList = async () => {
         throw error;
     }
 };
-
-// 창고 상세 정보 불러오기
-export const fetchWarehouseDetail = async (warehouseId) => {
-    const response = await axios.post(LOGISTICS_API.WAREHOUSE_DETAIL_API(warehouseId));
-    return response.data;  // 서버에서 받은 창고 상세 정보 반환
-};
-
-// 창고 정보 업데이트
-export const updateWarehouse = async (warehouseId, updatedData) => {
-    try {
-        const response = await axios.post(
-            LOGISTICS_API.WAREHOUSE_UPDATE_API(warehouseId),
-            {
-                ...updatedData,
-                hierarchyGroupList: updatedData.hierarchyGroupList.map(id => ({ id })),  // 계층 그룹 리스트 변환
-            }
-        );
-        return response.data;
-    } catch (error) {
-        console.error("창고 정보를 업데이트하는 중 오류 발생:", error);
-        throw error;
-    }
-};
