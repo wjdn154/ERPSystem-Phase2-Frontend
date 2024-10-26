aec63f6f6013b4ebca64311ee16c803fe4a296d0
Heeyoun Lim <heeyn.lim@gmail.com>
2024-10-27 00:44:19 +0900

    style: 공정 등록및수정 form

--- src/modules/production/basic_information_management/process_details_management/ProcessDetailsPage.jsx
+++ src/modules/production/basic_information_management/process_details_management/ProcessDetailsPage.jsx
@@ -2,16 +2,33 @@ import React, {useEffect, useMemo, useState} from 'react';
 import {Box, Grid, Grow, Paper} from '@mui/material';
 import WelcomeSection from '../../../../components/WelcomeSection.jsx';
 import {Typography} from '@mui/material';
-import {Button, Col, Modal, Row} from 'antd';
+import {Button, Col, Form, Modal, notification, Row, Table} from 'antd';
 import TemporarySection from "../../../../components/TemporarySection.jsx";
 import {useProcessDetails} from "./ProcessDetailsHook.jsx";
 import ProcessDetailsListSection from "./ProcessDetailsListSection.jsx";
 import SelectedProcessDetailsSection from "./SelectedProcessDetailsSection.jsx";
 import {getRowClassName, tabItems, processDetailsColumn} from "./ProcessDetailsUtil.jsx";
+import {useNotificationContext} from "../../../../config/NotificationContext.jsx";
+import {fetchWorkcenter} from "../workcenter_management/WorkcenterApi.jsx";
+import {fetchProcessDetail} from "./ProcessDetailsApi.jsx";
+import apiClient from "../../../../config/apiClient.jsx";
+import {PRODUCTION_API} from "../../../../config/apiConstants.jsx";
 
 const ProcessDetailsPage = ({ initialData }) => {
 
+    const notify = useNotificationContext(); // 알림 컨텍스트 사용
+    const [form] = Form.useForm(); // 폼 인스턴스 생성
+    const [registrationForm] = Form.useForm(); // 폼 인스턴스 생성
     const [activeTabKey, setActiveTabKey] = useState('1');
+    const [isModalVisible, setIsModalVisible] = useState(false); // 모달 상태 관리
+    const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+    const [isLoading, setIsLoading] = useState(false); // 로딩 상태
+    const [displayValues, setDisplayValues] = useState({});
+    const [processDetailsData, setProcessDetailsData] = useState(null); // 선택된 작업장 데이터 관리
+    const [processDetailsParam, setProcessDetailsParam] = useState({
+
+    }); // 선택된 작업장 데이터 관리
+
     const {
         data,
         processDetail,
@@ -34,6 +51,41 @@ const ProcessDetailsPage = ({ initialData }) => {
     const handleTabChange = (key) => {
         console.log("Tab 변경됨:", key);  // 디버그 로그 추가
         setActiveTabKey(key);
+    };
+
+    // 폼 제출 핸들러
+    const handleFormSubmit = async (values, type) => {
+        console.log('handleFormSubmit 호출됨. 폼 제출 값:', values);  // 전달된 값 확인
+
+        confirm({
+            title: '저장 확인',
+            content: '정말로 저장하시겠습니까?',
+            okText: '확인',
+            cancelText: '취소',
+            onOk: async () => {
+                try {
+                    if (type === 'register') {
+                        // API 요청: 새 작업장 등록
+                        await apiClient.post(PRODUCTION_API.SAVE_PROCESS_API, values);
+                        notify('success', '등록 성공', '공정이 등록되었습니다.', 'bottomRight');
+                    } else if (type === 'update') {
+                        // API 요청: 기존 작업장 수정
+                        await apiClient.post(PRODUCTION_API.UPDATE_PROCESS_API(values.code), values);
+                        notify('success', '등록 성공', '공정이 수정되었습니다.', 'bottomRight');
+                    }
+                } catch (error) {
+                    console.error('저장 실패:', error);
+                    notify('error', '저장 실패', '데이터 저장 중 오류가 발생했습니다.', 'top');
+                }
+            },
+            onCancel() {
+                notification.warning({
+                    message: '저장 취소',
+                    description: '저장이 취소되었습니다.',
+                    placement: 'bottomLeft',
+                });
+            },
+        });
     };
 
     return (
@@ -62,37 +114,62 @@ const ProcessDetailsPage = ({ initialData }) => {
                                 {/* 기본 데이터 목록 */}
                                 <Typography variant="h6" sx={{ padding: '20px' }} >생산공정 목록</Typography>
                                 <Grid sx={{ padding: '0px 20px 0px 20px' }}>
-                                    <Row gutter={16}
-                                         style={{marginTop: isSearchActive && searchData && searchData.length > 0 ? '16px' : '0'}}>
-                                        <Col span={24}>
-                                            <ProcessDetailsListSection
-                                                columns={processDetailsColumn}
-                                                data={data}
-                                                handleSelectedRow={handleSelectedRow}
-                                                rowClassName={getRowClassName}
-                                            />
-                                        </Col>
-                                    </Row>
-                                    {/* 모달 컴포넌트 */}
-                                    {processDetail && (
-                                        <Modal
-                                            open={isProcessModalVisible} // 모달 상태에 따라 표시
-                                            onCancel={handleClose} // 모달을 닫는 함수
-                                            footer={null} // 모달의 하단 버튼 제거
-                                        >
-                                            <SelectedProcessDetailsSection
-                                                processDetail={processDetail}
-                                                handleClose={handleClose}
-                                                handleInputChange={handleInputChange}
-                                                handleSave={handleSave}
-                                                handleDeleteProcessDetail={handleDeleteProcessDetail}
-                                            />
-                                        </Modal>
-                                    )}
+                                    <Table
+                                        columns={processDetailsColumn}
+                                        dataSource={data}
+                                        rowClassName={getRowClassName}
+                                        pagination={{ pageSize: 15, position: ['bottomCenter'], showSizeChanger: false }}
+                                        size="small"
+                                        rowKey="code"
+                                        rowSelection={{
+                                            type: 'radio', // 선택 방식 (radio or checkbox)
+                                            selectedRowKeys: selectedRowKeys, // 선택된 행의 키들
+                                            onChange: (newSelectedRowKeys) => {
+                                                console.log('새로운 Row Keys:', newSelectedRowKeys); // 디버그용 로그
+                                                setSelectedRowKeys(newSelectedRowKeys); // 선택된 행의 키 업데이트
+                                            },
+                                        }}
+                                        // onRow={(record) => ({
+                                        //     onClick: () => handleSelectedRow(record), // 행 클릭 시 해당 공정 선택
+                                        //     style: { cursor: 'pointer' },
+                                        // })}
+                                        onRow={(record) => ({
+                                            style: { cursor: 'pointer' },
+                                            onClick: async () => {
+                                                setSelectedRowKeys([record.code]); // 클릭한 행의 키로 상태 업데이트
+                                                try {
+                                                    // 공정 상세 정보 가져오기 (API 호출)
+                                                    const detail = await fetchProcessDetail([record.code]);
+
+                                                    setProcessDetailsData(detail); // 선택된 작업장 데이터 설정
+                                                    setProcessDetailsParam(detail);
+                                                    notify('success', '조회', '생산공정 정보 조회 성공.', 'bottomRight');
+                                                } catch (error) {
+                                                    console.error("생산공정 정보 조회 실패:", error);
+                                                    notify('error', '조회 오류', '생산공정 정보 조회 중 오류가 발생했습니다.', 'top');
+                                                }
+                                                // handleFormSubmit(record);
+                                            },
+                                        })}
+                                    />
                                 </Grid>
                             </Paper>
                         </Grow>
                     </Grid>
+                    {/* 생산공정 등록 및 수정 */}
+                    {processDetailsData && (
+                        <SelectedProcessDetailsSection
+                            key={processDetailsData.code}
+                            processDetail={processDetailsData}
+                            handleInputChange={handleInputChange}
+                            handleClose={() => setProcessDetailsData(null)} // 닫기 핸들러
+                            // handleSave={handleSave}
+                            handleSelectedRow={handleSelectedRow}
+                            handleDeleteProcessDetail={handleDeleteProcessDetail}
+                            rowClassName={getRowClassName}
+                            handleFormSubmit={handleFormSubmit}
+                        />
+                    )}
                 </Grid>
             )}
 
--- src/modules/production/basic_information_management/process_details_management/SelectedProcessDetailsSection.jsx
+++ src/modules/production/basic_information_management/process_details_management/SelectedProcessDetailsSection.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Button, Space, Modal, Form, Input, Select } from 'antd';
-import { ActionButtons, showDeleteConfirm} from "../../common/commonActions.jsx";
-import { isTrueOptions } from "../../common/dropdownOptions.jsx";
+import React, {useEffect, useState} from 'react';
+import { Space, Tag, Form, Table, Button, Col, Input, Row, Checkbox, Modal, DatePicker, Spin, Select, notification, Divider } from 'antd';
 
-const { confirm } = Modal;
+import { ActionButtons, showDeleteConfirm} from "../../common/commonActions.jsx";
+import {Box, Grid, Grow, Paper, Typography} from "@mui/material";
+import {useNotificationContext} from "../../../../config/NotificationContext.jsx";
+import {SearchOutlined} from "@ant-design/icons";
+import {LOGISTICS_API, PRODUCTION_API} from "../../../../config/apiConstants.jsx";
+import apiClient from "../../../../config/apiClient.jsx";
 
 const SelectedProcessDetailsSection = ({
-                                           processDetail,
+                                           // processDetail,
                                            handleInputChange,
                                            handleSave,
                                            handleDeleteProcessDetail,
                                        }) => {
+
+const [isModalVisible, setIsModalVisible] = useState(false); // 모달 활성화 여부 상태
+const [isLoading, setIsLoading] = useState(false); // 로딩 상태
+const [currentField, setCurrentField] = useState(''); // 모달 분기 할 필드 상태
+const [modalData, setModalData] = useState(null); // 모달 데이터 상태
+const [initialModalData, setInitialModalData] = useState(null);
+const [isEndDateDisable, setIsEndDateDisable] = useState(false); // 거래 종료일 비활성화 여부 상태
+const [displayValues, setDisplayValues] = useState({});
+const [processDetail, setProcessDetail] = useState({});
+const notify = useNotificationContext(); // 알림 컨텍스트 사용
+const [form] = Form.useForm(); // 폼 인스턴스 생성
+const [registrationForm] = Form.useForm(); // 폼 인스턴스 생성
+const [processDetailParam, setProcessDetailParam] = useState(false);
+
 
     // 삭제 확인 다이얼로그 호출
     const handleDelete = () => {
@@ -20,68 +37,156 @@ const SelectedProcessDetailsSection = ({
         );
     };
 
-    if (!processDetail) {
-        return <div>공정을 선택하면 상세 조회할 수 있습니다.</div>;
+    // 선택된 공정이 변경될 때마다 `form`에 값을 반영
+    useEffect(() => {
+        if (processDetail) {
+            form.setFieldsValue(processDetail); // 선택된 공정 데이터를 폼에 설정
+        }
+    }, [processDetail, form]);
+
+    const handleFormSubmit = (values) => {
+        console.log("handleFormSubmit", values);
     }
 
+    const handleModalCancel = () => setIsModalVisible(false);
+
+    // 모달창 데이터 가져오기 함수
+    const fetchModalData = async (fieldName) => {
+        setIsLoading(true);
+        let apiPath;
+
+        if (fieldName === 'workcenter') apiPath = PRODUCTION_API.WORKCENTER_LIST_API;
+        if (fieldName === 'factory') apiPath = LOGISTICS_API.WAREHOUSE_LIST_API;
+
+        try {
+            const response = await apiClient.post(apiPath);
+            setModalData(response.data);
+            setInitialModalData(response.data);
+            console.log('response.data:', response.data);
+        } catch (error) {
+            notify('error', '조회 오류', '데이터 조회 중 오류가 발생했습니다.', 'top');
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+// 모달창 선택 핸들러
+    const handleModalSelect = (record) => {
+        switch (currentField) {
+            case 'workcenter':
+                setProcessDetail((prevDetail) => ({
+                    ...prevDetail,
+                    workcenter: {
+                        code: record.code,
+                        name: record.name,
+                    },
+                }));
+                setDisplayValues((prevValues) => ({
+                    ...prevValues,
+                    workcenter: `[${record.code}] ${record.name}`,
+                }));
+                break;
+
+            case 'factory':
+                setProcessDetail((prevDetail) => ({
+                    ...prevDetail,
+                    factory: {
+                        code: record.code,
+                        name: record.name,
+                    },
+                }));
+                setDisplayValues((prevValues) => ({
+                    ...prevValues,
+                    factory: `[${record.code}] ${record.name}`,
+                }));
+                break;
+
+            default:
+                console.warn('알 수 없는 필드:', currentField);
+        }
+
+        // 모달창 닫기
+        setIsModalVisible(false);
+    };
+
     return (
-        <div style={{ padding: '20px', position: 'relative' }}>
-            {/* 공정 상세 정보를 표시하는 Form */}
-            <Form layout="vertical" initialValues={processDetail}>
-                <Form.Item label="공정 코드">
-                    <Input
-                        value={processDetail.code}
-                        onChange={(e) => handleInputChange(e, 'code')}
-                    />
-                </Form.Item>
-                <Form.Item label="공정 명">
-                    <Input
-                        value={processDetail.name}
-                        onChange={(e) => handleInputChange(e, 'name')}
-                    />
-                </Form.Item>
-                <Form.Item label="외주여부">
-                    <Select
-                        value={processDetail.isOutsourced ? 'Y' : 'N'}
-                        options={isTrueOptions}  // 공통 드롭다운 옵션 사용
-                        onChange={(value) => handleInputChange({ target: { value } }, 'isUsed')}
-                    />
-                </Form.Item>
-                <Form.Item label="소요시간">
-                    <Input
-                        value={processDetail.duration}
-                        onChange={(e) => handleInputChange(e, 'duration')}
-                    />
-                </Form.Item>
-                <Form.Item label="공정비용">
-                    <Input
-                        value={processDetail.cost}
-                        onChange={(e) => handleInputChange(e, 'cost')}
-                    />
-                </Form.Item>
-                <Form.Item label="불량률(%)">
-                    <Input
-                        value={processDetail.defectRate}
-                        onChange={(e) => handleInputChange(e, 'defectRate')}
-                    />
-                </Form.Item>
-                <Form.Item label="사용여부">
-                    <Select
-                        value={processDetail.isUsed}  // true 또는 false로 설정
-                        options={isTrueOptions}  // 공통 드롭다운 옵션 사용 (value: true/false)
-                        onChange={(value) => handleInputChange({ target: { value } }, 'isUsed')}
-                    />
-                </Form.Item>
+        <Grid item xs={12} md={12} sx={{ minWidth: '700px !important', maxWidth: '1200px !important',}}>
+            <Grow in={true} timeout={200}>
+                <Paper elevation={3} sx={{ height: '100%' }}>
+                    <Typography variant="h6" sx={{ padding: '20px' }} >공정 등록 및 수정</Typography>
+                    <Grow sx={{ padding: '0px 20px 0px 20px' }}>
+                        {/* 공정 상세 정보를 표시하는 Form */}
+                        <Form
+                            form={form}
+                            initialValues={processDetail}
+                            onFinish={handleSave}
+                            layout="vertical"
+                        >
+                            <Divider orientation="left" style={{ fontWeight: 600 }}>기초 정보</Divider>
+                            <Row gutter={16}>
+                                <Col span={8}>
+                                    <Form.Item name="code" label="공정코드" rules={[{ required: true }]}>
+                                        <Input readOnly />
+                                    </Form.Item>
+                                </Col>
+                                <Col span={8}>
+                                    <Form.Item name="name" label="공정명" rules={[{ required: true }]}>
+                                        <Input />
+                                    </Form.Item>
+                                </Col>
+                                <Col span={8}>
+                                    <Form.Item name="duration" label="소요시간" rules={[{ required: true }]}>
+                                        <Input type="number" />
+                                    </Form.Item>
+                                </Col>
+                            </Row>
 
-            </Form>
+                            <Divider orientation="left" style={{ fontWeight: 600 }}>추가 정보</Divider>
+                            <Row gutter={16}>
+                                <Col span={8}>
+                                    <Form.Item name="cost" label="비용" rules={[{ required: true }]}>
+                                        <Input type="number" />
+                                    </Form.Item>
+                                </Col>
+                                <Col span={8}>
+                                    <Form.Item name="isOutsourced" valuePropName="checked">
+                                        <Checkbox>외주여부</Checkbox>
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+                            <Space style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                                <Button type="primary" htmlType="submit">저장</Button>
+                                <Button onClick={handleModalCancel}>취소</Button>
+                            </Space>
 
-            {/* 공통 ActionButtons 컴포넌트 사용 */}
-            <ActionButtons
-                onSave={handleSave}
-                onDelete={handleDelete}
-            />
-        </div>
+                            <Modal
+                                open={isModalVisible}
+                                onCancel={handleModalCancel}
+                                footer={null}
+                            >
+                                <Input
+                                    placeholder="검색"
+                                    prefix={<SearchOutlined />}
+                                    onChange={(e) => fetchModalData(e.target.value)}
+                                />
+                                <Table
+                                    dataSource={modalData}
+                                    columns={[
+                                        { title: '코드', dataIndex: 'code', key: 'code' },
+                                        { title: '이름', dataIndex: 'name', key: 'name' },
+                                    ]}
+                                    rowKey="id"
+                                    onRow={(record) => ({
+                                        onClick: () => handleModalSelect(record),
+                                    })}
+                                />
+                            </Modal>
+                        </Form>
 
+                    </Grow>
+                </Paper>
+            </Grow>
+        </Grid>
     );
 };
 
